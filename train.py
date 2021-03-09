20a0ee9c0a9fe1281ea257160a05f69be356be7a
nexemjail <nexemjail@gmail.com>
2021-03-10 00:37:04 +0300

    Prettify

--- train.py
+++ train.py
@@ -1,61 +1,58 @@
-from sklearn.pipeline import Pipeline
+import os
+from argparse import ArgumentParser
 
-from sklearn.preprocessing import  OneHotEncoder
+import joblib
+import pandas as pd
+from lightgbm.sklearn import LGBMClassifier
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from lightgbm.sklearn import LGBMClassifier
-from argparse import ArgumentParser
-import pandas as pd
-import joblib
-import os
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 
 def train_model(data_path: str, save_model_path: str):
-    num_columns = ['Age', 'Fare']
-    median_imputer = SimpleImputer(strategy='median')
-    cat_columns = ['Sex', 'Embarked', 'Pclass']
-    ohe = OneHotEncoder(handle_unknown='ignore')
+    num_columns = ["Age", "Fare"]
+    median_imputer = SimpleImputer(strategy="median")
+    cat_columns = ["Sex", "Embarked", "Pclass"]
+    ohe = OneHotEncoder(handle_unknown="ignore")
     ct = ColumnTransformer(
         transformers=[
             (
-                'num_columns', median_imputer, num_columns,
+                "num_columns",
+                median_imputer,
+                num_columns,
             ),
-            (
-                'cat_columns', ohe, cat_columns
-            )
+            ("cat_columns", ohe, cat_columns),
         ]
-
     )
     pipeline = Pipeline(
         steps=[
-            ('preprocess', ct),
-            ('model', LGBMClassifier(n_estimators=150, verbose=True))
+            ("preprocess", ct),
+            ("model", LGBMClassifier(n_estimators=150, verbose=1)),
         ]
     )
 
-    train_df = pd.read_csv(os.path.join(data_path, 'train.csv'))
-    output_path = os.path.join(save_model_path, 'model.joblib')
-    pipeline.fit(
-        train_df.drop(columns=['Survived']),
-        train_df.Survived
-    )
+    train_df = pd.read_csv(os.path.join(data_path, "train.csv"))
+    pipeline.fit(train_df.drop(columns=["Survived"]), train_df.Survived)
 
+    output_path = os.path.join(save_model_path, "model.joblib")
     joblib.dump(pipeline, output_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ap = ArgumentParser()
-    ap.add_argument('--train_data_dir',
-                    type=str,
-                    default=os.environ.get('SM_CHANNEL_TRAIN'),
-                    )
+    ap.add_argument(
+        "--train_data_dir",
+        type=str,
+        default=os.environ.get("SM_CHANNEL_TRAIN"),
+    )
 
-    ap.add_argument('--model_path',
-                    type=str,
-                    default=os.environ.get('SM_MODEL_DIR'),
-                    )
+    ap.add_argument(
+        "--model_path",
+        type=str,
+        default=os.environ.get("SM_MODEL_DIR"),
+    )
 
     args = ap.parse_args()
 
-    train_model(data_path=args.train_data_dir,
-                save_model_path=args.model_path)
+    train_model(data_path=args.train_data_dir, save_model_path=args.model_path)
